d834314c0c2e4fd6f132c59d0a9936d38a9febb6
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-10-27 19:33:54 -0600

    add pageviews

--- crudmovies/urls.py
+++ crudmovies/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import indexPageView
+from .views import indexPageView, movieInfoPageView, addMoviePageView, addReviewPageView
 
 urlpatterns = [
-    path('', indexPageView, name='index')
+    path('', indexPageView, name='index'),
+    path('movie/<str:movieID>', movieInfoPageView, name='movieInfoPageView'),
+    path('movie/<str:movieID>/editMovie', addMoviePageView, name='addMoviePageView'),
+    path('movie/<str:movieID>/editReview/<int:reviewID>/', addReviewPageView, name='addReviewPageView'),
 ]
--- crudmovies/views.py
+++ crudmovies/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def indexPageView():
+def indexPageView(request):
     return HttpResponse('Index')
+
+def movieInfoPageView(request):
+    return HttpResponse('MovieInfoPageView')
+
+def addMoviePageView(request):
+    return HttpResponse('addMoviePageView')
+
+def addReviewPageView(request):
+    return HttpResponse('addReviewPageView')
