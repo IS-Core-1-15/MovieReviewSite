7cdb8312bc985f7da543009d76e9116b4caefbcd
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-11-15 09:30:14 -0700

    Add models

--- crudmovies/models.py
+++ crudmovies/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.username
+
+class Movie(models.Model):
+    title = models.CharField(max_length=30)
+    duration = models.DurationField()
+    release_date = models.DateField()
+    director = models.CharField(max_length=60)
+    main_photo = models.ImageField(upload_to='photos')
+
+    movie_reviews = models.ManyToManyField('User', through='Review')
+
+    def __str__(self):
+        return f'{self.title} by {self.director}'
+
+class Review(models.Model):
+    user_id = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    movie_id = models.ForeignKey(Movie, null=True, blank=True, on_delete=models.SET_NULL)
+    rating = models.IntegerField(max_length=1)
+    description = models.TextField(max_length=500)
+    review_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.user_id}: {self.movie_id}-{self.rating}'
--- moviereviewsite/settings.py
+++ moviereviewsite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*s*44(y-eo8=n(=5q8zo)p8zefai1**6+t!p_r#iazw&%im*k5'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))  
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,8 +79,11 @@ WSGI_APPLICATION = 'moviereviewsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crud_movie',
+        'USER': 'postgres',
+        'PASSWORD': str(os.getenv('DB_PASSWORD')),
+        'HOST': 'localhost'
     }
 }
 
