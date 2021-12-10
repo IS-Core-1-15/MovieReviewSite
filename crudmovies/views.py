9c9ab92e450d4f8ba2e0a59cb033811ce2be6d1b
zuuab <zacharymartinheaton@gmail.com>
2021-12-10 12:40:06 -0700

    still working on addmovie, formatting is good now

--- crudmovies/views.py
+++ crudmovies/views.py
@@ -8,6 +8,8 @@ from .models import *
 
 # Create your views here.
 # List all the movies in the DB
+
+
 def indexPageView(request):
     movies = Movie.objects.all()
     context = {
@@ -16,6 +18,8 @@ def indexPageView(request):
     return render(request, 'crudmovies/index.html', context)
 
 # show single movie info
+
+
 def moviePageView(request, movieTitle, movieYear):
     movie = Movie.objects.filter(title=movieTitle, release_year=movieYear)
     # reviews = Review.objects.filter(movie_id=movie[0].id)
@@ -34,6 +38,7 @@ def addPageView(request):
     elif request.method == 'POST':
         key = request.POST['title']
         movie = searchMovie(key)
+
         if movie.title == '':
             context = {
                 'msg': 'We could not find your movie, try another one!'
@@ -41,23 +46,29 @@ def addPageView(request):
         else:
             context = {
                 'msg': 'Is this the movie you\'re looking for?',
+                'msg2': 'If not, make sure it is spelled correctly or try searching for a different movie',
                 'movie': movie
             }
         for attr, value in movie.__dict__.items():
             print(f"{attr}: {value}")
         return render(request, 'crudmovies/addmovie.html', context)
 
+
 def deleteMoviePageView(request):
     return render(request, 'crudmovies/addmovie.html')
+
 
 def joinusPageView(request):
     return render(request, 'crudmovies/joinus.html')
 
+
 def aboutPageView(request):
     return render(request, 'crudmovies/about.html')
 
+
 def contactPageView(request):
     return render(request, 'crudmovies/contact.html')
+
 
 def searchPageView(request):
     return HttpResponse('Search')
@@ -67,11 +78,11 @@ def listPageView(request):
     return HttpResponse('List')
 
 
-def editReviewView(request) :
+def editReviewView(request):
     return HttpResponse('Edit Review')
 
 
-def deleteReviewView(request) :
+def deleteReviewView(request):
     return HttpResponse('Delete Review')
 
 
