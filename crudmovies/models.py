2e33e4cc6356a2e7a94bafe3bc84657a69302f20
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-12-12 15:28:06 -0700

    fix auto field for null value

--- crudmovies/models.py
+++ crudmovies/models.py
@@ -33,7 +33,7 @@ class Movie(models.Model):
  
 
 class Review(models.Model):
-    review_id = models.IntegerField(primary_key=True)
+    review_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=30)
     movie = models.ForeignKey(
         'Movie',
